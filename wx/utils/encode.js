71626f8ca5034386a4f462da7c698c15b032e6ee
hejiayi <hejiayi@kp99.cn>
2022-09-27 10:45:20 +0800

    feat: 编码格式改为GB18030

--- wx/utils/encode.js
+++ wx/utils/encode.js
@@ -3,7 +3,7 @@ import { TextEncoder } from '../libs/text-encode/encoding';
  * 中文转ArrayBuffer
  */
 
-// unicode 转 ArrayBuffer
+// unicode 转 ArrayBuffer，适用于没有中文的场景
 function unicode2ArrayBuffer(str) {
   const out = new ArrayBuffer(str.length);
   const uint8 = new Uint8Array(out);
@@ -16,9 +16,9 @@ function unicode2ArrayBuffer(str) {
   return out;
 }
 
-// GBK 转 ArrayBuffer
+// GB18030 转 ArrayBuffer，国内大部分打印机都是用GB18030
 function gbk2buffer(content) {
-  const encoder = new TextEncoder('gb2312', {
+  const encoder = new TextEncoder('gb18030', {
     NONSTANDARD_allowLegacyEncoding: true,
   });
   const val = encoder.encode(content);
